fe94152bddb67a68dda21541fab3784788dc72ce
3cg1 <3cg1@S30214>
2021-10-12 14:41:22 +0200

    Dodaj pliki projektów.

---
+++ ppo12101/ppo12101.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+ofstream MyFile;
+int main() {
+    string linia;
+    fstream plik;
+    int x;
+    int y;
+    plik.open("wsp.txt", ios::in);
+    if (plik.good() == true)
+    {
+        while (!plik.eof())
+        {
+            plik >> x >> y;
+            cout << "\nx= " << x << "\ty= " << y;
+        }
+    };
+    MyFile.open("wsp2.txt");
+    MyFile << "tutaj jakaś informacja";
+    MyFile.close();
+    plik.close();
+
+}
